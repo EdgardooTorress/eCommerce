de2a78e8b05be51584f3575cf4f819b235d92240
Edgardo Torres <ejtf99usa@gmail.com>
2025-08-07 20:59:02 -0400

    fetchin json file and fixing some stuff

--- contacus/contactus.js
+++ contacus/contactus.js
@@ -1,37 +1,3 @@
-window.onload = function() {
-    fetch("products.json")
-      .then((response) => response.json())
-      .then((data) => {
-        let productsHtml = "";
-        for (let i = 0; i < data.length; i++) {
-
-          let item = data[i];
-  
-          let product = `
-            <div class="fragance"${i+1}>
-            <img src="${item.img}" alt="productimg" class="images">
-            <h1>${item.name}</h1>
-            <h3>${item.description}</h3>
-            <p>${item.price}</p>
-            <button>Add to Cart</button>
-            </div>
-          `;
-
-          productsHtml += product;
-        }
-
-        let productsContainer = document.getElementsByClassName("box")[0]
-
-        productsContainer.innerHTML = productsHtml;
-      })
-      
-      .catch((error) => {
-        console.error("Error fetching data from json file:", error);
-      });
-
-};
-
-
 const Days = document.getElementById('days');
 const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
